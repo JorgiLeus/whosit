7e9c6bc4a393df5fdacc9be6011d66ef8633f322
Bram Lemenu <bram.lemenu@me.com>
2018-08-31 14:09:56 +0200

    Created models

--- src/main/java/com/axxes/whosit/WhositApplication.java
+++ src/main/java/com/axxes/whosit/WhositApplication.java
@@ -1,9 +1,13 @@
 package com.axxes.whosit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.axxes")
+@EnableAutoConfiguration
 public class WhositApplication {
 
 	public static void main(String[] args) {
