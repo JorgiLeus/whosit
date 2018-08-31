753962bf50465790d33c1ec246b89182f1afc289
JorgiLeus <38461447+JorgiLeus@users.noreply.github.com>
2018-08-31 09:36:21 +0200

    InitialCommit

---
+++ src/main/java/com/axxes/whosit/WhositApplication.java
@@ -0,0 +1,12 @@
+package com.axxes.whosit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhositApplication.class, args);
+	}
+}
